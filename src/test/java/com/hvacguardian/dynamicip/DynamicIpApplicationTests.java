5083224d8e53f74d2cbeb72fbc243f8f22de44a7
Jesus <jesus@movesfinancial.com>
2023-11-14 18:53:48 -0500

    first commit

---
+++ src/test/java/com/hvacguardian/dynamicip/DynamicIpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvacguardian.dynamicip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicIpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
